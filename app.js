325ec5d2403f59bf7c248b1fbf1ac2f36212e68d
YuZheLin1987 <billlinxx@gmail.com>
2022-10-13 00:57:07 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 // require packages and set variables
 const express = require('express')
+const exphbs = require('express-handlebars')
 const app = express()
 const port = 3000
 
+// template engine setting
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
 // route setting
 app.get('/', (req, res) => {
-  res.send('This is from index.')
+  res.render('index')
 })
 
 // start and listen server
