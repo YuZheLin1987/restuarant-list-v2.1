e0013e665880f6960fbb04d92d1f298c21051b44
YuZheLin1987 <billlinxx@gmail.com>
2022-10-14 20:26:50 +0800

    refactor: use json file to render restaurants

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // require packages and set variables
 const express = require('express')
 const exphbs = require('express-handlebars')
+const restaurants = require('./restaurant.json')
 const app = express()
 const port = 3000
 
@@ -13,7 +14,7 @@ app.use(express.static('public'))
 
 // route setting
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('index', { restaurants: restaurants.results })
 })
 
 // start and listen server
