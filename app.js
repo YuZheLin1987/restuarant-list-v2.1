39960157f98847d8e4cbca0a2d417bfde961a5c3
YuZheLin1987 <billlinxx@gmail.com>
2022-10-15 09:20:15 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -4,6 +4,20 @@ const exphbs = require('express-handlebars')
 const restaurants = require('./restaurant.json')
 const app = express()
 const port = 3000
+const mongoose = require('mongoose')
+
+// connect to database
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 // template engine setting
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
