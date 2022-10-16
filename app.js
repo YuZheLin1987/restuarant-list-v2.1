eb65d6d3b9a2e68138dd315217dfbabcd9a159b3
YuZheLin1987 <billlinxx@gmail.com>
2022-10-16 11:22:21 +0800

    feat: add create restaurant function

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const Restaurant = require('./models/restaurant')
 const app = express()
 const port = 3000
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 
 // connect to database
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -26,11 +27,33 @@ app.set('view engine', 'handlebars')
 
 // static files import
 app.use(express.static('public'))
+// use body-parser
+app.use(bodyParser.urlencoded({ extended: true }))
 
 // route setting
 // index page
 app.get('/', (req, res) => {
   Restaurant.find().lean().then(restaurant => res.render('index', { restaurant })).catch(error => console.error)
+})
+
+// new page
+app.get('/restaurants/new', (req, res) => {
+  return res.render('new')
+})
+
+app.post('/restaurants', (req, res) => {
+  const id = req.body.id
+  const name = req.body.name
+  const name_en = req.body.name_en
+  const category = req.body.category
+  const image = req.body.image
+  const location = req.body.location
+  const phone = req.body.phone
+  const google_map = req.body.google_map
+  const rating = req.body.rating
+  const description = req.body.description
+
+  return Restaurant.create({ id, name, name_en, category, image, location, phone, google_map, rating, description }).then(() => res.redirect('/')).catch(error => console.log(error))
 })
 
 // show page
